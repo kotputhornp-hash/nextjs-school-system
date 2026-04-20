89cb27579ac2e73cb93bd5c72b59a479d684ccc6
kotputhornp-hash <kotputhorn_p@old.silpakorn.edu>
2026-04-20 12:54:03 +0700

    RESET_EVERYTHING_FORCE_DEPLOY

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,5 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  output: 'standalone', // เพิ่มเพื่อให้ Vercel จัดการไฟล์ได้ดีขึ้น
 };
-
 export default nextConfig;
